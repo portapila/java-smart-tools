88f7eedbd1592615a488c4fe318414969ff6603d
Portapila <portapila@gmail.com>
2022-11-21 15:53:47 -0300

    just add some comments and organize the code

--- src/javasmarttools/MainPackageUseExamples.java
+++ src/javasmarttools/MainPackageUseExamples.java
@@ -5,30 +5,29 @@ import java.util.Random;
 public class MainPackageUseExamples {
 
 	public static void main(String[] args) throws InterruptedException {
-		
+
 		Stopwatch myWatch = new Stopwatch();
 		Random myRandom = new Random();
-		
+
 		int laps = 5 + myRandom.nextInt(5); // select a number from 5 to 10
-		
+
 		System.out.println("Horses will run for " + laps + " laps");
 
 		myWatch.reset();
-		
+
 		for (int i = 0; i < laps; i++) {
 
 			int thisDelay = 500 + myRandom.nextInt(1000);
-			
-			System.out.print("Lap #" + (i + 1) + ". The horse is running... (wait)..");
-			Thread.sleep(thisDelay);	
-			System.out.println("The last lap took " + myWatch.markLapDown() + "ms. Total Elapsed time is " + myWatch.getElapsedTime() + "ms.");
-		}
-		
-		Thread.sleep(60*1000);	
-		myWatch.stop();
-		System.out.println("Total running time = " + myWatch.getElapsedTime() + "ms (˜" + myWatch.getElapsedTime() / 1000 + "s).");
 
-		
+			System.out.print("Lap #" + (i + 1) + ". The horse is running... (wait)...");
+			Thread.sleep(thisDelay);
+			System.out.println("The last lap took " + myWatch.markLapDown() + "ms. Total Elapsed time is "
+					+ myWatch.getElapsedTime() + "ms.");
+		}
+
+		myWatch.stop();
+		System.out.println(
+				"Total running time = " + myWatch.getElapsedTime() + "ms (˜" + myWatch.getElapsedTime() / 1000 + "s).");
 
 	}
 
--- src/javasmarttools/Stopwatch.java
+++ src/javasmarttools/Stopwatch.java
@@ -1,62 +1,60 @@
 package javasmarttools;
 
 public class Stopwatch {
-	
-	protected
-	  long stopwatchStartedTime = 0,
-		   stopwatchElapsedTime = 0,
-		   
-	       lapStartedTime = 0,
-	       lapElapsedTime = 0;
-	
+
+	protected long stopwatchStartedTime = 0, stopwatchElapsedTime = 0,
+
+			lapStartedTime = 0, lapElapsedTime = 0;
+
 	/*
 	 * Stopwatch functions
 	 */
 
+	// reset all the counters
 	public void reset() {
 		this.stopwatchStartedTime = this.lapStartedTime = getCurrentTime();
 		this.stopwatchElapsedTime = this.lapElapsedTime = 0L;
 	}
-	
+
+	// stop main timer (stopwatch timer)
 	public long stop() {
 		this.markLapDown();
 		return stopwatchElapsedTime;
 	}
-	
-	// returns elapsed time in the last lapMarkDown
+
+	// returns elapsed stopwatch time since last 'lapMarkDown' or 'stop'
 	public long getElapsedTime() {
 		return stopwatchElapsedTime;
-		
 	}
-	
+
 	/*
 	 * Lap Functions
 	 */
 
+	// save last lap time
 	public long markLapDown() {
 		long currentTime = this.getCurrentTime();
-		
 		stopwatchElapsedTime = currentTime - stopwatchStartedTime;
 		lapElapsedTime = currentTime - lapStartedTime;
 		lapStartedTime = currentTime;
-		
 		return this.getLastLap();
 	}
-	
+
+	// get last lap time
 	public long getLastLap() {
 		return lapElapsedTime;
-		
+
 	}
-	
+
 	/*
 	 * Internal + control Functions
 	 */
 
-
+	// starts timer when object is created - can be reset on 'reset' method
 	public Stopwatch() {
 		this.reset();
 	}
-	
+
 	protected long getCurrentTime() {
 		return System.currentTimeMillis();
 	}
